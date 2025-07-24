aa367c2b506aeeb8f9a904c824671796bb847bed
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-24 11:29:55 +0530

    Update Implement Queue using Arrays.py

--- Stack and Queue/Implement Queue using Arrays.py
+++ Stack and Queue/Implement Queue using Arrays.py
@@ -1,30 +1,30 @@
 class ArrayQueue(object):
-  def __init__(self):
-    self.cap=1000
-    self.q=[0]*self.cap
-    self.start=0
-    self.end=0
-    self.size=0
+    def __init__(self):
+        self.cap=1000
+        self.q=[0]*self.cap
+        self.start=0
+        self.end=0
+        self.size=0
 
-  def push(self,x):
-    if self.size==self.cap:
-      return
-    self.q[self.end]=x
-    self.end=(self.end+1)%self.cap
-    self.size+=1
+    def push(self,x):
+        if self.size==self.cap:
+            return
+        self.q[self.end]=x
+        self.end=(self.end+1)%self.cap
+        self.size+=1
 
-  def pop(self):
-    if self.isEmpty():
-      return -1
-    val=self.q[self.start]
-    self.start=(self.start+1)%self.cap
-    self.size-=1
-    return val
+    def pop(self):
+        if self.isEmpty():
+            return -1
+        val=self.q[self.start]
+        self.start=(self.start+1)%self.cap
+        self.size-=1
+        return val
 
-  def peek(self):
-    if self.isEmpty():
-      return -1
-    return self.q[self.start]
+    def peek(self):
+        if self.isEmpty():
+          return -1
+        return self.q[self.start]
 
-  def isEmpty(self):
-    return self.size==0
+    def isEmpty(self):
+        return self.size==0
