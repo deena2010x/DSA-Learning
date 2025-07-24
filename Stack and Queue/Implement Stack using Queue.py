d838ca808be48a4bbeb55f35f829d1929e5d1de4
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-24 11:26:48 +0530

    Create Implement Stack using Queue.py

---
+++ Stack and Queue/Implement Stack using Queue.py
@@ -0,0 +1,18 @@
+class QueueStack(object):
+  def __init__(self):
+    self.q=deque()
+
+  def push(self,x):
+    n=len(self.q)
+    self.q.append(x)
+    for i in range(n):
+      self.q.append(self.q.popleft())
+
+  def pop(self):
+    return self.q.popleft()
+
+  def top(self):
+    return self.q[0]
+
+  def isEmpty(self):
+    return not self.q
