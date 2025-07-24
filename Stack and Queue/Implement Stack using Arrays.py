e2b920928876078875e318d376e2206fd2cf2622
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-24 11:14:58 +0530

    Update Implement Stack using Arrays.py

--- Stack and Queue/Implement Stack using Arrays.py
+++ Stack and Queue/Implement Stack using Arrays.py
@@ -1,22 +1,22 @@
-class ArrayStack:
+class ArrayStack(object):
     def __init__(self):
         self.cap=1000
         self.st=[0]*self.cap
         self.top=-1
-    
+
     def push(self,x):
         if self.top==self.cap-1:
             return
         self.top+=1
         self.st[self.top]=x
-    
+
     def pop(self):
         if self.isEmpty():
             return -1
         val=self.st[self.top]
         self.top-=1
         return val
-    
+
     def peek(self):
         if self.isEmpty():
             return -1
