a2d64b508d0c5120afc77f31b12998ec6bec03bf
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-25 12:29:15 +0530

    Create Implement queue using Linkedlist.py

---
+++ Stack and Queue/Implement queue using Linkedlist.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+
+class LinkedListQueue:
+    def __init__(self):
+        self.start=None
+        self.end=None
+
+    def push(self,x):
+        new=Node(x)
+        if self.end:
+            self.end.next=new
+        else:
+            self.start=new
+        self.end=new
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        val=self.start.val
+        self.start=self.start.next
+        if self.start==None:
+            self.end=None
+        return val
+
+    def top(self):
+        if self.isEmpty():
+            return None
+        return self.start.val
+
+    def isEmpty(self):
+        return self.start==None
