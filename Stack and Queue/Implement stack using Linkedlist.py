c4ece8af88233309b758105b489b5aa1429d3ad1
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-25 12:20:34 +0530

    Create Implement stack using Linkedlist.py

---
+++ Stack and Queue/Implement stack using Linkedlist.py
@@ -0,0 +1,27 @@
+class Node:
+    self.val=val
+    self.next=None
+
+class LinkedListStack:
+    def __init__(self,val):
+        self.top_node=None
+
+    def push(self,x):
+        new=Node(x)
+        new.next=self.top_node
+        self.top_node=new
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        val=self.top_node.val
+        self.top_node=self.top_node.next
+        return val
+
+    def top(self):
+        if self.isEmpty():
+            return None
+        return self.top_node.val
+
+    def isEmpty(self):
+        return self.top_node==None
