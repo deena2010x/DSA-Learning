08f676254b6ca85b09d895ae78434b82457eb4dd
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-26 06:57:05 +0530

    Create Postfix to Prefix Conversion.py

---
+++ Stack and Queue/Postfix to Prefix Conversion.py
@@ -0,0 +1,12 @@
+class Solutiion(object):
+    def postfixToPrefix(self,s):
+        n=len(s)
+        st=[]
+        for i in range(n):
+            if s[i].isalnum():
+                st.append(s[i])
+            else:
+                a=st.pop()
+                b=st.pop()
+                st.append(s[i]+a+b)
+        return st[-1]
