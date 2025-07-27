595d78703e404a39059927737807310a6fdd50db
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-27 18:18:10 +0530

    Create Sum of subarray ranges.py

---
+++ Stack and Queue/Sum of subarray ranges.py
@@ -0,0 +1,70 @@
+class Solution:
+    def subArrayRanges(self,nums):
+        def pse(a):
+            n=len(a)
+            st=[]
+            list1=[0]*n
+            for i in range(n):
+                while st and a[st[-1]]>a[i]:
+                    st.pop()
+                if st:
+                    list1[i]=st[-1]
+                else:
+                    list1[i]=-1
+                st.append(i)
+            return list1
+        def nse(a):
+            n=len(a)
+            st=[]
+            list1=[0]*n
+            for i in range(n-1,-1,-1):
+                while st and a[st[-1]]>=a[i]:
+                    st.pop()
+                if st:
+                    list1[i]=st[-1]
+                else:
+                    list1[i]=n
+                st.append(i)
+            return list1
+        def pge(a):
+            n=len(a)
+            st=[]
+            list1=[0]*n
+            for i in range(n):
+                while st and a[st[-1]]<a[i]:
+                    st.pop()
+                if st:
+                    list1[i]=st[-1]
+                else:
+                    list1[i]=-1
+                st.append(i)
+            return list1
+        def nge(a):
+            n=len(a)
+            st=[]
+            list1=[0]*n
+            for i in range(n-1,-1,-1):
+                while st and a[st[-1]]<=a[i]:
+                    st.pop()
+                if st:
+                    list1[i]=st[-1]
+                else:
+                    list1[i]=n
+                st.append(i)
+            return list1
+        n=len(nums)
+        prev_min=pse(nums)
+        next_min=nse(nums)
+        prev_max=pge(nums)
+        next_max=nge(nums)
+        total_min=0
+        total_max=0
+        for i in range(n):
+            left_min=i-prev_min[i]
+            right_min=next_min[i]-i
+            total_min+=nums[i]*left_min*right_min
+            left_max=i-prev_max[i]
+            right_max=next_max[i]-i
+            total_max+=nums[i]*left_max*right_max
+            total=total_max-total_min
+        return total
