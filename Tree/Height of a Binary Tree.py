25417e2139f9fc8576d70c44c6c0ec46bd00d1f9
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-30 14:03:32 +0530

    Create Height of a Binary Tree.py

---
+++ Tree/Height of a Binary Tree.py
@@ -0,0 +1,12 @@
+class TreeNode(object):
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution(object):
+    def maxDepth(self,root):
+        if not root:
+            return 0
+        lh=self.maxDepth(root.left)
+        rh=self.maxDepth(root.right)
+        return 1+max(lh,rh)
