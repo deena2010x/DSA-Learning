6aaf3179f374569d988f0f9e11ed7e3889f40e00
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-30 13:09:18 +0530

    Create Iterative Preorder Traversal of Binary Tree.py

---
+++ Tree/Iterative Preorder Traversal of Binary Tree.py
@@ -0,0 +1,19 @@
+class TreeNode(object):
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution(object):
+    def preorder(self,root):
+        if not root:
+            return []
+        preorder=[]
+        stack=[root]
+        while stack:
+            node=stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
