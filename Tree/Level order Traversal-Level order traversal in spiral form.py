77b4eed9bea1b9e728c1b3aec44e4e5eca615d2a
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-07-30 13:04:57 +0530

    Create Level order Traversal-Level order traversal in spiral form.py

---
+++ Tree/Level order Traversal-Level order traversal in spiral form.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution(object):
+    def levelorder(self,root):
+        def dfs(node,level):
+            if not node:
+                return
+            if level==len(list1):
+                list1.append([])
+            list1[level].append(node.val)
+            dfs(node.left,level+1)
+            dfs(node.right,level+1)
+        list1=[]
+        dfs(root,0)
+        return list1
