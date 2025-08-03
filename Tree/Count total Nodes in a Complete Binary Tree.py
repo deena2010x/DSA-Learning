04ea58519e7750461c151bd5fb4864edae2bdc8b
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-08-03 22:25:45 +0530

    Update Count total Nodes in a Complete Binary Tree.py

--- Tree/Count total Nodes in a Complete Binary Tree.py
+++ Tree/Count total Nodes in a Complete Binary Tree.py
@@ -5,6 +5,22 @@ class TreeNode(object):
         self.right=right
 class Solution(object):
     def countNodes(self,root):
+        def leftHeight(node):
+            h=0
+            while node:
+                h+=1
+                node=node.left
+            return h
+        def rightHeight(node):
+            h=0
+            while node:
+                h+=1
+                node=node.right
+            return h
         if not root:
             return 0
+        l=leftHeight(root)
+        r=rightHeight(root)
+        if l==r:
+            return (1<<l)-1
         return 1+self.countNodes(root.left)+self.countNodes(root.right)
