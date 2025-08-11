3a1947c074bf35d7b500eed8e83efcee44e62094
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-08-11 08:04:05 +0530

    Create Shortest Path in UG with unit weights.py

---
+++ Graph/Shortest Path in UG with unit weights.py
@@ -0,0 +1,20 @@
+from collections import deque
+class Solution:
+    def shortestPath(self,edges,N,M):
+        graph=[[] for i in range(N)]
+        for i,j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+        list1=[float('inf')]*N
+        list1[0]=0
+        queue=deque([0])
+        while queue:
+            node=queue.popleft()
+            for i in graph[node]:
+                if list1[node]+1<list1[i]:
+                    list1[i]=list1[node]+1
+                    queue.append(i)
+        for i in range(N):
+            if list1[i]==float('inf'):
+                list1[i]=-1
+        return list1
