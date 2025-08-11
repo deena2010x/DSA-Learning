4bae8aa5f27e193e0e48e7f6845fae538969ffb4
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-08-11 08:57:50 +0530

    Update Shortest path in a binary maze.py

--- Graph/Shortest path in a binary maze.py
+++ Graph/Shortest path in a binary maze.py
@@ -4,12 +4,12 @@ class Solution(object):
         n=len(matrix)
         if matrix[0][0]!=0 or matrix[n-1][n-1]!=0:
             return -1
-        queue=deque([[0,0,1]])
+        queue=deque([[1,0,0]])
         vis=[[0]*n for i in range(n)]
         vis[0][0]=1
         d=[[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
         while queue:
-            r,c,d1=queue.popleft()
+            d1,r,c=queue.popleft()
             if r==n-1 and c==n-1:
                 return d1
             for dr,dc in d:
@@ -18,5 +18,5 @@ class Solution(object):
                 if row<0 or row>=n or col<0 or col>=n or vis[row][col] or matrix[row][col]!=0:
                     continue
                 vis[row][col]=1
-                queue.append([row,col,d1+1])
+                queue.append([d1+1,row,col])
         return -1
