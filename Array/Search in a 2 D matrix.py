84261e2365c6969c3d9537f2e3995d3265af4006
Deena Patel <130888233+deena2010x@users.noreply.github.com>
2025-09-16 08:45:51 +0530

    Create Search in a 2 D matrix.py

---
+++ Array/Search in a 2 D matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self,matrix,target):
+        m=len(matrix)
+        n=len(matrix[0])
+        l=0
+        h=m*n-1
+        while l<=h:
+            m1=(l+h)//2
+            r=m1//n
+            c=m1%n
+            if matrix[r][c]==target:
+                return True
+            elif matrix[r][c]<target:
+                l=m1+1
+            else:
+                h=m1-1
+        return False
